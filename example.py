3e708d2fae4695e441f3b0a1ead54ac95a8398f4
Marco Trinelli (mtrinell) <mtrinell@cisco.com>
2025-02-10 10:22:13 +0100

    make things configurable

--- example.py
+++ example.py
@@ -4,12 +4,18 @@ import asyncio
 import os
 from dotenv import load_dotenv
 
+
 async def main():
-    res = await graph.ainvoke(
-        {"messages": [("user", "Who is the founder of LangChain?")]},
-        {"configurable": {"system_prompt": "You are a helpful AI assistant."}},
-    )
-    print(res)
+    config = {"configurable": {
+        "system_prompt": "Answer in italian",
+        "model": "azure_openai/gpt-4o-mini"
+    }}
+
+    async for event in graph.astream({"messages": [("user", "Who is the founder of LangChain?")]}, config=config):
+        for k, v in event.items():
+            if k != "__end__":
+                print(k, v)
+                print()
 
 if __name__ == "__main__":
     asyncio.run(main())
--- src/react_agent/graph.py
+++ src/react_agent/graph.py
@@ -17,6 +17,7 @@ from langgraph.prebuilt import ToolNode
 from react_agent.configuration import Configuration
 from react_agent.state import InputState, State
 from react_agent.tools import TOOLS
+from react_agent.utils import load_chat_model
 
 from langchain_openai import AzureChatOpenAI
 
@@ -41,12 +42,7 @@ async def call_model(
     configuration = Configuration.from_runnable_config(config)
 
     # Initialize the model with tool binding. Change the model or add more tools here.
-    # model = load_chat_model(configuration.model).bind_tools(TOOLS)
-
-    model = AzureChatOpenAI(
-        model=os.getenv("AZURE_OPENAI_MODEL", ''),
-        temperature=0.0
-    ).bind_tools(TOOLS)
+    model = load_chat_model(configuration.model).bind_tools(TOOLS)
 
     # Format the system prompt. Customize this to change the agent's behavior.
     system_message = configuration.system_prompt.format(
